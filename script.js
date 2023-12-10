03b7f021e6624bade06b8452aa69832aeb972d40
chetan-581 <chetangautam@gmail.com>
2023-12-10 22:09:13 +0530

    Initial Commit of homepage

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var nav = document.querySelector('.nav');
+    nav.classList.toggle('open');
+
+    var bar = document.querySelector('.icons');
+    bar.classList.toggle('open');
+}
